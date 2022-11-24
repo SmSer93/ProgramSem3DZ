a375c612dc19574684ba676447342d423fa245df
Sergey <segarik93@gmail.com>
2022-11-24 22:40:30 +0300

    DZ.Task3.end

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Task № 21 
-
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
 
 Console.WriteLine("Введите координаты точки А  ->");
 Console.Write("X1: ");
@@ -17,12 +19,12 @@ int yb = Convert.ToInt32(Console.ReadLine());
 Console.Write("Z2: ");
 int zb = Convert.ToInt32(Console.ReadLine());
 
-double Distance (int xa, int ya, int za, int xb, int yb, int zb)
+double Distance(int xa, int ya, int za, int xb, int yb, int zb)
 {
-    return Math.Sqrt((xb - xa)*(xb - xa) + (yb - ya)*(yb - ya) + (zb - za)*(zb - za));
+    return Math.Sqrt((xb - xa) * (xb - xa) + (yb - ya) * (yb - ya) + (zb - za) * (zb - za));
 }
 
-double res = Math.Round(Distance (xa, ya, za, xb, yb, zb), 2);
+double res = Math.Round(Distance(xa, ya, za, xb, yb, zb), 2);
 Console.WriteLine(res);
 
 
---
+++ Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿// Task 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int num1 = 0, num2 = 0, num3 = 0, num4 = 0, num5 = 0;
+int n = number;
+
+if (number > 9999 && number < 100000)
+{
+    num1 = n % 10;
+    n = n / 10;
+    num2 = n % 10;
+    n = n / 10;
+    num3 = n % 10;
+    n = n / 10;
+    num4 = n % 10;
+    num5 = n / 10;
+    if (num1 == num5 && num2 == num4)
+    {
+        Console.WriteLine($"{number} --> Да");
+    }
+
+    else { Console.WriteLine($"{number} --> Нет"); }
+}
+else Console.WriteLine("Введено не пятизначное число");
