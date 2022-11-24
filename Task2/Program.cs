34b319ce2a53c15d6fe38479a1c5ba1e69afaf90
Sergey <segarik93@gmail.com>
2022-11-24 22:19:53 +0300

    DZ.Task2.End

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task № 23 
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+
+Console.WriteLine("Введите натуральное число -> ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int num)
+{
+    int num1 = 1;
+    while (num1 <= num)
+    {
+        int cube = num1 * num1 * num1;
+        Console.Write($"{num1} | {cube} ");
+        Console.WriteLine();
+        num1++;
+    }
+}
+if (number > 0)
+    Cube(number);
+else Console.WriteLine("Число является не натуральным");
