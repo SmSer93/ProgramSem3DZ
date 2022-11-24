d572d6eab1cf7766745de8f69549f4282e1ba215
Sergey <segarik93@gmail.com>
2022-11-24 21:37:12 +0300

    DZ.Task1.End

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите координаты точки А  ->");
+Console.Write("X1: ");
+double xa = Convert.ToDouble(Console.ReadLine());
+Console.Write("Y1: ");
+double ya = Convert.ToDouble(Console.ReadLine());
+Console.Write("Z1: ");
+double za = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B  ->");
+Console.Write("X2: ");
+double xb = Convert.ToDouble(Console.ReadLine());
+Console.Write("Y2: ");
+double yb = Convert.ToDouble(Console.ReadLine());
+Console.Write("Z2: ");
+double zb = Convert.ToDouble(Console.ReadLine());
+
+double range = Math.Sqrt((xb - xa)*(xb - xa) + (yb - ya)*(yb - ya) + (zb - za)*(zb - za));
+double res = Math.Round(range , 2);
+Console.WriteLine(res);
+
+
+
+
